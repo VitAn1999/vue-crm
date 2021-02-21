1e1b2b35e7a7a88b830ae5f2afd403841963d23e
Рощупкин Виталий <elpistolero1999@gmail.com>
2021-02-21 15:40:44 +0300

    install Vuelidate and add validations in Login.vue

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{ open: value }">
+    <router-link
+      tag="li"
+      active-class="active"
+      v-for="link in links"
+      :key="link.url"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  data() {
+    return {
+      links: [
+        {
+          title: 'Счет',
+          url: '/',
+          exact: true,
+        },
+        {
+          title: 'История',
+          url: '/history',
+          exact: false,
+        },
+        {
+          title: 'Планирование',
+          url: '/planning',
+          exact: false,
+        },
+        {
+          title: 'Новая запись',
+          url: '/record',
+          exact: false,
+        },
+        {
+          title: 'Категории',
+          url: '/categories',
+          exact: false,
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,55 @@
 <template>
-  <form class="card auth-card">
+  <form @submit.prevent="onSubmit" class="card auth-card">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input id="email" type="text" class="validate" />
+        <input
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{
+            invalid:
+              ($v.email.$dirty && !$v.email.required) ||
+              ($v.email.$dirty && !$v.email.email),
+          }"
+        />
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+          v-if="$v.email.$dirty && !$v.email.required"
+          class="helper-text invalid"
+          >Введите ваш email</small
+        >
+        <small
+          v-else-if="$v.email.$dirty && !$v.email.email"
+          class="helper-text invalid"
+          >Введенный email не корректный</small
+        >
       </div>
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input
+          id="password"
+          type="password"
+          v-model.trim="password"
+          :class="{
+            invalid:
+              ($v.password.$dirty && !$v.password.required) ||
+              ($v.password.$dirty && !$v.password.minLength),
+          }"
+          password
+        />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+          v-if="$v.password.$dirty && !$v.password.required"
+          class="helper-text invalid"
+          >Введите свой пароль</small
+        >
+        <small
+          v-else-if="$v.password.$dirty && !$v.password.email"
+          class="helper-text invalid"
+          >Введенный пароль должен содержать не менее
+          {{ $v.password.$params.minLength.min }} символов. Сейчас введено
+          {{ this.password.length }}</small
+        >
       </div>
     </div>
     <div class="card-action">
@@ -23,8 +62,45 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/registration">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
+
+<script>
+import { email, required, minLength } from 'vuelidate/lib/validators';
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  validations: {
+    email: {
+      email,
+      required,
+    },
+    password: {
+      required,
+      minLength: minLength(6),
+    },
+  },
+  methods: {
+    onSubmit() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      const login = {
+        email: this.email,
+        password: this.password,
+      };
+      console.log(login);
+      this.$router.push('/');
+    },
+  },
+};
+</script>
